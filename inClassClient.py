7f1a1fe30dec3a3abbd80777abed7fe37ffe55e8
Joseph Eggers <joseph.eggers@csu.fullerton.edu>
2023-12-08 11:14:55 -0800

    working version last night

--- inClassClient.py
+++ inClassClient.py
@@ -1,15 +1,9 @@
 from mrjob.job import MRJob
-from mrjob.step import MRStep
 from mrjob.protocol import TextProtocol
 import sys
 
 class JoinClientJob(MRJob):
     OUTPUT_PROTOCOL = TextProtocol
-
-    def steps(self):
-        return [
-            MRStep(mapper=self.mapper, reducer=self.reducer)
-        ]
 
     def mapper(self, _, value):
         itr = value.split()
